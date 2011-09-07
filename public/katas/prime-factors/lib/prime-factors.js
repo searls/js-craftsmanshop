980083ce8c70d0f5affc4ed0f79f4840902894cd
Justin Searls <searls@gmail.com>
2011-09-07 19:00:59 -0400

    wrote the first spec and made it pass

---
+++ public/katas/prime-factors/lib/prime-factors.js
@@ -0,0 +1,5 @@
+(function() {
+  window.primeFactors = function() {
+    return [];
+  };
+})();
---
+++ public/katas/prime-factors/spec/prime-factors-spec.js
@@ -0,0 +1,5 @@
+describe("#primeFactors", function() {
+  it("finds no factors for 1", function() {
+    expect(primeFactors(1)).toEqual([]);
+  });
+});
