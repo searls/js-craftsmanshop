bc3fca29898ac74391e9863769d3351e037ce7be
Justin Searls <searls@gmail.com>
2011-09-07 19:02:17 -0400

    wrote a new failing spec, b/c it's no fun to start work green.

--- public/katas/prime-factors/spec/prime-factors-spec.js
+++ public/katas/prime-factors/spec/prime-factors-spec.js
@@ -2,4 +2,7 @@ describe("#primeFactors", function() {
   it("finds no factors for 1", function() {
     expect(primeFactors(1)).toEqual([]);
   });
+  it("finds 2 as a prime factor of 2", function() {
+    expect(primeFactors(2)).toEqual([2]);
+  });
 });
